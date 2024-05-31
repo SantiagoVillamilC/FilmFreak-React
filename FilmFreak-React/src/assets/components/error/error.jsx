a1665b5a69c51fe78d305f5b5d3990a17235ac57
SantiagoVillamilC <santyago4993@gmail.com>
2024-05-31 16:10:51 -0500

    other pages

--- FilmFreak-React/src/assets/components/error/error.jsx
+++ FilmFreak-React/src/assets/components/error/error.jsx
@@ -4,7 +4,7 @@ export const NotFound = ()=>{
     return(
         <>
             <h1> NotFound Error 404. Page </h1>
-            <img src={"https://httpcats.com/404.jpg"} width={"100%"} height={"800px"} />
+            <img src={"https://httpcats.com/404.jpg"} width={"50vw"} height={"auto"} />
         </>
     )
 }
